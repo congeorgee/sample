1cd7492bbd10626f48af53279abc3e3a2e8a27c1
george <abhilash.george@gbst.com>
2016-08-30 17:12:05 +0800

    first commit

---
+++ CodeTesting/src/com/Test.java
@@ -0,0 +1,27 @@
+/*
+ * ####################################################<br/>
+ *   Copyright © GBST Limited (UK Company No: 816145) 1995-2016<br/>
+ * ####################################################<br/>
+ * Creation date: Aug 30, 2016<br/>
+ * Creation Time: 5:03:08 PM<br/>
+ * @author AbhilashG<br/>
+ */
+package com;
+
+/**
+ * ####################################################<br/>
+ *   Copyright © GBST Limited (UK Company No: 816145) 1995-2016<br/>
+ * ####################################################<br/>
+ * Creation date: Aug 30, 2016<br/>
+ * Creation Time: 5:03:08 PM<br/>
+ * @author AbhilashG<br/>
+ */
+public class Test {
+	
+	public static void main(String []args)
+	{
+		System.out.println("First Sample");
+	}
+
+	
+}
